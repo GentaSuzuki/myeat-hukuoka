af1e6864c4a38bfb5c8bd43b315aa64c3783949f
GentaSuzuki <71617008+GentaSuzuki@users.noreply.github.com>
2020-10-21 11:55:12 +0900

    モデルバリデーション

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+   
+         with_options presence: true do
+          validates :nickname
+          validates :email,format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, uniqueness: {case_sensitive: false}}
+          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid. Input half-width characters.' }
+          validates :encrypted_password
+          validates :years_old
+          end
+  
+          has_many :items
+          has_many :orders
+
+end
