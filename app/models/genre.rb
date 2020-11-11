6b079de6002bef021ccb75cc9f55f1c48cb0a916
GentaSuzuki <71617008+GentaSuzuki@users.noreply.github.com>
2020-11-11 11:25:24 +0900

    ジャンル追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -5,11 +5,13 @@ class Genre < ActiveHash::Base
     { id: 2, data: '洋食・西洋料理' },
     { id: 3, data: '中華料理' },
     { id: 4, data: 'アジア・エスニック' },
-    { id: 5, data: '焼肉' },
-    { id: 6, data: '鍋' },
-    { id: 7, data: 'ラーメン' },
-    { id: 8, data: 'パン・サンドイッチ' },
-    { id: 9, data: 'カフェ' },
-    { id: 10, data: 'スイーツ' }
+    { id: 5, data: 'カレー'}
+    { id: 6, data: '焼肉' },
+    { id: 7, data: '鍋' },
+    { id: 8, data: 'ラーメン' },
+    { id: 9, data: 'パン・サンドイッチ' },
+    { id: 10, data: 'カフェ' },
+    { id: 11, data: '居酒屋'},
+    { id: 12, data: 'スイーツ' }
   ]
 end
